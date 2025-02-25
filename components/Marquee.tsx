33d6e79290269ee045f94dcc50d1974cb8c6fda1
Mauricio Gutierrez <142355026+AMGutierrezTrejos@users.noreply.github.com>
2025-02-24 23:36:55 -0500

    Implementing Frames

--- components/Marquee.tsx
+++ components/Marquee.tsx
@@ -1,24 +1,46 @@
 /* eslint-disable prettier/prettier */
 import { Image, ScrollView, View } from 'react-native';
+import Animated, {
+  SharedValue,
+  useAnimatedStyle,
+  useFrameCallback,
+  useSharedValue,
+} from 'react-native-reanimated';
 
 const itemWidth = 250;
 
-function MarqueeItem({ event, index }: { event: any; index: number }) {
+type MarqueeItemProps = { event: any; index: number; scroll: SharedValue<number> };
+
+function MarqueeItem({ event, index, scroll }: MarqueeItemProps) {
   const initialPosition = itemWidth * index;
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      left: initialPosition - scroll.value,
+    };
+  });
+
   return (
-    <View
-      className="absolute h-full bg-purple-700/50 p-5 shadow-md"
-      style={{ left: initialPosition, width: itemWidth }}>
+    <Animated.View
+      className="absolute h-full  p-5 shadow-md"
+      style={[{ width: itemWidth }, animatedStyle]}>
       <Image source={event.image} className="h-full w-full rounded-3xl" />
-    </View>
+    </Animated.View>
   );
 }
 
 export default function Marquee({ events }: { events: any[] }) {
+  const scroll = useSharedValue(0);
+  const scrollSpeed = 1;
+
+  useFrameCallback(() => {
+    scroll.value = scroll.value + scrollSpeed;
+  });
+
   return (
     <View className="h-full flex-row">
       {events.map((event, index) => (
-        <MarqueeItem key={event.id} event={event} index={index} />
+        <MarqueeItem key={event.id} event={event} index={index} scroll={scroll} />
       ))}
     </View>
   );
